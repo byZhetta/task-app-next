07e36bfa8a047c9785aee83e11a7df7cdc87e857
ignaciozhetta <zagu4988@gmail.com>
2023-02-09 15:20:17 -0300

    [feat] creating/listing tasks

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,15 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { conn } from "../../../utils/database";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
     
-    const { method } = req;
+    const { method, body } = req;
 
     switch (method) {
         case 'GET':
-            return res.status(200).json("getting tasks");
+            try {
+                const query = 'SELECT * FROM tasks';
+                const response = await conn.query(query);
+                return res.status(200).json(response.rows);
+            } catch (error: any) {
+                return res.status(400).json({error: error.message});
+            }
         case 'POST':
-            return res.status(200).json("creating tasks");
+            try {
+                const {title, description} = body;
+                const query = 'INSERT INTO tasks(title, description) VALUES ($1, $2) RETURNING *';
+                const values = [title, description];
+                const response = await conn.query(query, values);
+                return res.status(200).json(response.rows[0]);
+            } catch (error: any) {
+                return res.status(400).json({error: error.message});
+            }
         default:
             return res.status(400).json("invalid method");
     }

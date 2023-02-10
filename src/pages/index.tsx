fa309838f7c5986ca068bb7a969489e9f40b283b
ignaciozhetta <zagu4988@gmail.com>
2023-02-10 12:19:28 -0300

    [feat] SemanticUI added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
-export default function index() {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
+import {Task} from "src/interfaces/Task";
+
+interface Props {
+  tasks: Task[]
 }
+4
+export default function index({ tasks }: Props) {
+  return <>{ tasks.length === 0 ? <h1>no tasks</h1> : <h1>tasks</h1> }</>;
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch('http://localhost:3000/api/tasks')
+  const tasks = await res.json()
+  return {
+    props: {
+      tasks: tasks,
+    },
+  };
+};

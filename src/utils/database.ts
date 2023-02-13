13e8af90369dfe92c7947dc434445057a53ce48e
ignaciozhetta <zagu4988@gmail.com>
2023-02-13 11:37:04 -0300

    [chore] dotenv added

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,11 +4,11 @@ let conn: any;
 
 if (!conn) {
     conn = new Pool ({
-        user: 'postgres',
-        password: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        database: 'taskdb'
+        user: process.env.USER_NAME,
+        password: process.env.DB_PASSWORD,
+        host: process.env.HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DATABASE
     });
 }
 
